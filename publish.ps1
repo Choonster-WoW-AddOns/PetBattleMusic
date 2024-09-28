ddb46f9280af13cc10a3f64a94810a595c735ea8
Choonster TheMage <Choonster.2010@gmail.com>
2024-09-29 01:42:42 +1000

    Improve publish script - Prevent music and config files from being overwritten - Use -SkipTocCreation flag from PublishAddon fork

--- publish.ps1
+++ publish.ps1
@@ -2,11 +2,73 @@
 .SYNOPSIS
     Packages the AddOn with the BigWigs Packager and copies it to the WoW game directories.
     
-    Requires https://github.com/Tuller/PublishAddon to be installed in ~\source\repos\PublishAddon.
+    Requires https://github.com/Choonster-WoW-AddOns/PublishAddon to be installed in ~\source\repos\PublishAddon.
 #>
+
+$ErrorActionPreference = 'Stop'
 
 $global:WOW_HOME = 'D:\World of Warcraft'
 
+[string] $AddOnDir = Join-Path $global:WOW_HOME '_retail_\Interface\AddOns\PetBattleMusic'
+[string] $BackupDir = "$AddOnDir.backup"
+[string[]] $PathsToBackup = @('Music', 'ogginfo', 'config.lua', 'music.lua')
+
 Import-Module '~\source\repos\PublishAddon\wow.psm1'
 
-Publish-Addon
+function Copy-BackupItem {
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $RelativePath,
+        [Parameter(Mandatory)]
+        [string]
+        $SourceDir,
+        [Parameter(Mandatory)]
+        [string]
+        $DestinationDir,
+        [Parameter(Mandatory)]
+        [string]
+        $Message
+    )
+    
+    $sourcePath = Join-Path $SourceDir $RelativePath
+    $destinationPath = Join-Path $DestinationDir $RelativePath
+
+    $isDirectory = Test-Path -LiteralPath $sourcePath -PathType Container
+
+    if ($isDirectory -and (Test-Path $destinationPath)) {
+        Remove-Item -LiteralPath $destinationPath -Recurse
+    }
+
+    Copy-Item -Path $sourcePath -Destination $destinationPath -Recurse:$isDirectory
+
+    Write-Host $Message
+}
+
+[bool]$ShouldBackup = Test-Path -LiteralPath $AddOnDir
+
+if ($ShouldBackup) {
+    if (-not (Test-Path -LiteralPath $BackupDir)) {
+        New-Item -ItemType Directory -Path $BackupDir | Out-Null
+    }
+
+    $PathsToBackup | ForEach-Object {
+        Copy-BackupItem `
+            -RelativePath $_ `
+            -SourceDir $AddOnDir `
+            -DestinationDir $BackupDir `
+            -Message "Backed up $_"
+    }
+}
+
+Publish-Addon -Flavor Retail -SkipTocCreation
+
+if ($ShouldBackup) {
+    $PathsToBackup | ForEach-Object {
+        Copy-BackupItem `
+            -RelativePath $_ `
+            -SourceDir $BackupDir `
+            -DestinationDir $AddOnDir `
+            -Message "Restored $_"
+    }
+}
